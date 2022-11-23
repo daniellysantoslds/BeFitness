beec2a9a9260245090b55105ea5ddf4c928ac630
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2022-11-23 10:02:56 -0300

    teste da nova branch

--- motiva Watch App/ContentView.swift
+++ motiva Watch App/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("testeeee")
+            Text("testeeeeeeeste")
         }
         .padding()
     }
