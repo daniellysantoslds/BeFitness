13af7964ae41d30b00fe242f8df70569f3c33c9b
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2022-11-23 09:30:48 -0300

    PRIMEIRO COMMIT

---
+++ motiva Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  motiva Watch App
+//
+//  Created by Danielly Santos Lopes da Silva on 23/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("primeiro commit")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ motiva Watch App/motivaApp.swift
@@ -0,0 +1,17 @@
+//
+//  motivaApp.swift
+//  motiva Watch App
+//
+//  Created by Danielly Santos Lopes da Silva on 23/11/22.
+//
+
+import SwiftUI
+
+@main
+struct motiva_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
