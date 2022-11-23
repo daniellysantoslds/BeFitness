922f47a16508947664ffb54fb7d041715aeabbb6
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2022-11-23 09:35:57 -0300

    teste

--- motiva Watch App/ContentView.swift
+++ motiva Watch App/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("primeiro commit")
+            Text("teste")
         }
         .padding()
     }
