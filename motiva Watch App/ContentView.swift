d8de75336547e73e690154f7f8ebdb55c3036026
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2022-11-24 09:59:10 -0300

    teste

--- motiva Watch App/ContentView.swift
+++ motiva Watch App/ContentView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "glossddddbe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("testeeeeeeeste")
+            Text("caminhos")
         }
         .padding()
     }
