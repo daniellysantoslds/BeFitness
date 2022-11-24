2544da40df5961ef655a4421980aaaa8f25d0b99
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2022-11-24 10:14:18 -0300

    criando modelo do botaoPrincipal

---
+++ motiva Watch App/Componentes/BotaoPrincipalComponenteView.swift
@@ -0,0 +1,38 @@
+//
+//  BotaoPrincipalComponenteView.swift
+//  motiva Watch App
+//
+//  Created by Danielly Santos Lopes da Silva on 24/11/22.
+//
+
+import SwiftUI
+
+struct BotaoPrincipalComponenteView: View {
+    let texto: String;
+    let corTexto: Color;
+    
+    
+    init(texto: String, corTexto: Color) {
+        self.texto = texto
+        self.corTexto = corTexto
+
+    }
+    
+    
+    var body: some View {
+        Button {
+            
+        } label: {
+            Text(texto)
+                .foregroundColor(corTexto)
+                
+        }
+
+    }
+}
+
+struct BotaoPrincipalComponenteView_Previews: PreviewProvider {
+    static var previews: some View {
+        BotaoPrincipalComponenteView(texto: "teste", corTexto: Color.white)
+    }
+}
---
+++ motiva Watch App/MenuPrincipal.swift
@@ -0,0 +1,20 @@
+//
+//  MenuPrincipal.swift
+//  motiva Watch App
+//
+//  Created by Danielly Santos Lopes da Silva on 24/11/22.
+//
+
+import SwiftUI
+
+struct MenuPrincipal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MenuPrincipal_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuPrincipal()
+    }
+}
