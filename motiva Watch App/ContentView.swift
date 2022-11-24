7ffdcca6d212ba8668777c1f122cfa0a4a1e782a
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2022-11-24 11:32:22 -0300

    tirando o texto git

--- motiva Watch App/ContentView.swift
+++ motiva Watch App/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("testeeeeeeeste")
-git         }
+     }
         .padding()
     }
 }
