5db8e4017b3bf80c1f87182812580bf41af62945
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2022-11-24 10:59:46 -0300

    botoes e navegacao

--- motiva Watch App/Componentes/BotaoPrincipalComponenteView.swift
+++ motiva Watch App/Componentes/BotaoPrincipalComponenteView.swift
@@ -1,38 +1,38 @@
+////
+////  BotaoPrincipalComponenteView.swift
+////  motiva Watch App
+////
+////  Created by Danielly Santos Lopes da Silva on 24/11/22.
+////
 //
-//  BotaoPrincipalComponenteView.swift
-//  motiva Watch App
+//import SwiftUI
 //
-//  Created by Danielly Santos Lopes da Silva on 24/11/22.
+//struct BotaoPrincipalComponenteView: View {
+//    let texto: String;
+//    let corTexto: Color;
+//    
+//    
+//    init(texto: String, corTexto: Color) {
+//        self.texto = texto
+//        self.corTexto = corTexto
 //
-
-import SwiftUI
-
-struct BotaoPrincipalComponenteView: View {
-    let texto: String;
-    let corTexto: Color;
-    
-    
-    init(texto: String, corTexto: Color) {
-        self.texto = texto
-        self.corTexto = corTexto
-
-    }
-    
-    
-    var body: some View {
-        Button {
-            
-        } label: {
-            Text(texto)
-                .foregroundColor(corTexto)
-                
-        }
-
-    }
-}
-
-struct BotaoPrincipalComponenteView_Previews: PreviewProvider {
-    static var previews: some View {
-        BotaoPrincipalComponenteView(texto: "teste", corTexto: Color.white)
-    }
-}
+//    }
+//    
+//    
+//    var body: some View {
+//        Button {
+//            print("printouuuu")
+//        } label: {
+//            Text(texto)
+//                .foregroundColor(corTexto)
+//                
+//        }
+//
+//    }
+//}
+//
+//struct BotaoPrincipalComponenteView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        BotaoPrincipalComponenteView(texto: "teste", corTexto: Color.white)
+//    }
+//}
--- motiva Watch App/motivaApp.swift
+++ motiva Watch App/motivaApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct motiva_Watch_AppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
+            MenuPrincipalView()
         }
     }
 }
