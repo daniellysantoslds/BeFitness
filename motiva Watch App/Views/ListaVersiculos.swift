f99cbf94c26becff6674bef11d7f9f2b2579ec40
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2022-11-25 08:52:12 -0300

    primeiro commit da branch

---
+++ motiva Watch App/Views/ListaVersiculos.swift
@@ -0,0 +1,20 @@
+//
+//  ListaVersiculos.swift
+//  motiva Watch App
+//
+//  Created by Danielly Santos Lopes da Silva on 25/11/22.
+//
+
+import SwiftUI
+
+struct ListaVersiculos: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListaVersiculos_Previews: PreviewProvider {
+    static var previews: some View {
+        ListaVersiculos()
+    }
+}
