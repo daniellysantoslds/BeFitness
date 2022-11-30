898747c9815f0d546bac1fee9369952ef480f7e0
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2022-11-29 21:20:58 -0300

    subindo a view humor

---
+++ motiva Watch App/Views/HumorView.swift
@@ -0,0 +1,20 @@
+//
+//  HumorView.swift
+//  motiva Watch App
+//
+//  Created by Danielly Santos Lopes da Silva on 29/11/22.
+//
+
+import SwiftUI
+
+struct HumorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HumorView_Previews: PreviewProvider {
+    static var previews: some View {
+        HumorView()
+    }
+}
