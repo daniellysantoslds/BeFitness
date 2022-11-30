9540fdd54f6acb548ad3c4831b47acd997f27229
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2022-11-30 08:12:32 -0300

    inserindo dados na view

--- motiva Watch App/Views/HumorView.swift
+++ motiva Watch App/Views/HumorView.swift
@@ -2,19 +2,56 @@
 //  HumorView.swift
 //  motiva Watch App
 //
-//  Created by Danielly Santos Lopes da Silva on 29/11/22.
+//  Created by Danielly Santos Lopes da Silva on 28/11/22.
 //
 
 import SwiftUI
 
 struct HumorView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack(spacing: 10){
+                VStack {
+                    Text("Como esta se sentindo?")
+                    
+                }
+                
+                HStack(spacing: 20) {
+                    
+                    
+                    NavigationLink (destination: MenuPrincipalView()) {
+                        Image(systemName: "heart")
+                    }.frame(width: 50, height: 50)
+                        .cornerRadius(16)
+                    
+                   
+                    NavigationLink (destination: MenuPrincipalView()) {
+                        Image(systemName: "heart")
+                    }.frame(width: 50, height: 50)
+                        .cornerRadius(16)
+                    
+                    NavigationLink (destination: MenuPrincipalView()) {
+                        Image(systemName: "heart")
+                    }.frame(width: 50, height: 50)
+                        .cornerRadius(16)
+                    
+                    
+                }
+            }
+            
+            
+            
+        }
+        
     }
 }
+
+
+
 
 struct HumorView_Previews: PreviewProvider {
     static var previews: some View {
         HumorView()
     }
 }
+
