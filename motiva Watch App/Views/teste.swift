f4f175b46ea739ffeded6eb90bda2646a92e9707
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2022-11-29 21:07:18 -0300

    up

---
+++ motiva Watch App/Views/teste.swift
@@ -0,0 +1,20 @@
+//
+//  teste.swift
+//  motiva Watch App
+//
+//  Created by Danielly Santos Lopes da Silva on 29/11/22.
+//
+
+import SwiftUI
+
+struct teste: View {
+    var body: some View {
+        Text("aaaaa")
+    }
+}
+
+struct teste_Previews: PreviewProvider {
+    static var previews: some View {
+        teste()
+    }
+}
