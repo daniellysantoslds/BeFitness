5292e5a008b8ff029749881e5cd3687bdef7f794
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2022-12-01 16:09:54 -0300

    up

--- motiva Watch App/Views/ListaVersiculosView.swift
+++ motiva Watch App/Views/ListaVersiculosView.swift
@@ -13,10 +13,15 @@ struct ListaVersiculosView: View {
             List(versiculos) { versiculo in
                 NavigationLink(destination:
                                 DetalhesVersiculoView(versiculo: versiculo)) {
-                    VStack {
+                    ZStack {
                         Text("\(versiculo.tituloVersiculo)")
+                            .foregroundColor(.black)
+                        
+                        
+                        
                     }
-                }
+                    
+                }.listRowBackground(versiculo.corBackground)
                 
             }
             
@@ -36,25 +41,27 @@ struct ListItem: Identifiable {
     var tituloVersiculo: String
     var descricaoVersiculo: String
     var turno : String
+    var cortexto: Color
+    var corBackground: Color
 }
 
 var versiculos = [
     ListItem(tituloVersiculo: "Acorda meninaaa",
-             descricaoVersiculo: "Bom dia, não deixe para amanhã o que pode ser feito hoje! Sabia que  praticar esportes pela manhã é uma maneira de melhorar o ânimo para o resto do dia. ", turno: "Manhã"),
+             descricaoVersiculo: "Bom dia, não deixe para amanhã o que pode ser feito hoje! Sabia que  praticar esportes pela manhã é uma maneira de melhorar o ânimo para o resto do dia. ", turno: "Manhã", cortexto: Color.black, corBackground: Color("corfeliz")),
     
-    ListItem(tituloVersiculo: "Olha o por do sol",
-             descricaoVersiculo: "Ta um pouco estressado? Suar um pouco pode te ajudar ! Voce sabia que durante a tarde voce ja ingeriu  duas grandes refeições e acumulou um bom aporte de energia, o que possibilita manter boa intensidade no exercício e otimizar resultados, tanto para o emagrecimento quanto para o aumento de massa muscular.", turno: "Tarde"),
-    
-    ListItem(tituloVersiculo: "A mimir",
-             descricaoVersiculo: "Ta na hora de desacelerar, tenta se desconectar um pouco do mundo agitado para ter uma boa noite de sono, você sabia que Dormir é mais importante que se exercitar?Se, para se exercitar de manhã, for preciso fazer um corte dolorido nas horas de sono, é melhor dormir mesmo uma horinha a mais e encaixar seus treinos na hora do almoço ou após o trabalho.", turno: "Noite"),
-    ListItem(tituloVersiculo: "Acorda meninaaa",
-             descricaoVersiculo: "Bom dia, hoje é um belo dia para nao deixar para a próxima segunda aquilo que pode ser feito hoje! Sabia que  praticar esportes pela manhã é uma maneira de melhorar o ânimo para o dia que virá pela frente.", turno: "Manhã"),
-    
-    ListItem(tituloVersiculo: "Olha o por do sol",
-             descricaoVersiculo: "Ta um pouco estressado? Suar um pouco pode te ajudar ! Voce sabia que durante a tarde voce ja ingeriu  duas grandes refeições e acumulou um bom aporte de energia, o que possibilita manter boa intensidade no exercício e otimizar resultados, tanto para o emagrecimento quanto para o aumento de massa muscular.", turno: "Tarde"),
-    
-    ListItem(tituloVersiculo: "A mimir",
-             descricaoVersiculo: "Ta na hora de desacelerar, tenta se desconectar um pouco do mundo agitado para ter uma boa noite de sono, você sabia que Dormir é mais importante que se exercitar?Se, para se exercitar de manhã, for preciso fazer um corte dolorido nas horas de sono, é melhor dormir mesmo uma horinha a mais e encaixar seus treinos na hora do almoço ou após o trabalho.", turno: "Noite"),
+//    ListItem(tituloVersiculo: "Olha o por do sol",
+//             descricaoVersiculo: "Ta um pouco estressado? Suar um pouco pode te ajudar ! Voce sabia que durante a tarde voce ja ingeriu  duas grandes refeições e acumulou um bom aporte de energia, o que possibilita manter boa intensidade no exercício e otimizar resultados, tanto para o emagrecimento quanto para o aumento de massa muscular.", turno: "Tarde"),
+//
+//    ListItem(tituloVersiculo: "A mimir",
+//             descricaoVersiculo: "Ta na hora de desacelerar, tenta se desconectar um pouco do mundo agitado para ter uma boa noite de sono, você sabia que Dormir é mais importante que se exercitar?Se, para se exercitar de manhã, for preciso fazer um corte dolorido nas horas de sono, é melhor dormir mesmo uma horinha a mais e encaixar seus treinos na hora do almoço ou após o trabalho.", turno: "Noite"),
+//    ListItem(tituloVersiculo: "Acorda meninaaa",
+//             descricaoVersiculo: "Bom dia, hoje é um belo dia para nao deixar para a próxima segunda aquilo que pode ser feito hoje! Sabia que  praticar esportes pela manhã é uma maneira de melhorar o ânimo para o dia que virá pela frente.", turno: "Manhã"),
+//
+//    ListItem(tituloVersiculo: "Olha o por do sol",
+//             descricaoVersiculo: "Ta um pouco estressado? Suar um pouco pode te ajudar ! Voce sabia que durante a tarde voce ja ingeriu  duas grandes refeições e acumulou um bom aporte de energia, o que possibilita manter boa intensidade no exercício e otimizar resultados, tanto para o emagrecimento quanto para o aumento de massa muscular.", turno: "Tarde"),
+//
+//    ListItem(tituloVersiculo: "A mimir",
+//             descricaoVersiculo: "Ta na hora de desacelerar, tenta se desconectar um pouco do mundo agitado para ter uma boa noite de sono, você sabia que Dormir é mais importante que se exercitar?Se, para se exercitar de manhã, for preciso fazer um corte dolorido nas horas de sono, é melhor dormir mesmo uma horinha a mais e encaixar seus treinos na hora do almoço ou após o trabalho.", turno: "Noite"),
     
     
     
---
+++ motiva Watch App/Views/TESTE.swift
@@ -0,0 +1,26 @@
+//
+//  TESTE.swift
+//  motiva Watch App
+//
+//  Created by Danielly Santos Lopes da Silva on 01/12/22.
+//
+
+import SwiftUI
+
+struct TESTE: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        
+        
+        List {
+            Text("OIIIX")
+        }
+    }
+}
+
+struct TESTE_Previews: PreviewProvider {
+    static var previews: some View {
+        TESTE()
+    }
+}
