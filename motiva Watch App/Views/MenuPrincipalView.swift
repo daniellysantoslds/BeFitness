03c57e75dd39b7ece6388d3cf4173f1a789f0148
Jpsmor <jpedrosmoreira1409@gmail.com>
2022-12-01 13:58:03 -0300

    update

--- motiva Watch App/Views/MenuPrincipalView.swift
+++ motiva Watch App/Views/MenuPrincipalView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 struct MenuPrincipalView: View {
     var body: some View {
@@ -25,14 +26,43 @@ struct MenuPrincipalView: View {
             }
             
             
-        }.onAppear(perform: noMoreOnboarding)
+        }.onAppear(perform: {
+            noMoreOnboarding()
+            setNotifications()
+        })
         
     }
     
-    func noMoreOnboarding () {
+    func noMoreOnboarding (){
         UserDefaults.standard.set(true, forKey: "OnboardingViewed")
     }
-    
+    func setNotifications() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.sound,.badge]) { (success, error) in
+            if success{
+                morningNotification()
+                afternoonNotification()
+                nightNotification()
+                print("All set")
+            } else if let error = error {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    func morningNotification() {
+        print("a")
+        let contentMorning = UNMutableNotificationContent()
+        contentMorning.title = versiculos[UserDefaults.standard.integer(forKey: "MorningKey")].tituloVersiculo
+        contentMorning.subtitle = versiculos[UserDefaults.standard.integer(forKey: "MorningKey")].descricaoVersiculo
+        contentMorning.sound = .default
+        contentMorning.categoryIdentifier = "myCategory"
+        UserDefaults.standard.set(UserDefaults.standard.integer(forKey: "MorningKey") + 3, forKey: "MorningKey")
+    }
+    func afternoonNotification() {
+        print("b")
+    }
+    func nightNotification() {
+        print("c")
+    }
 }
 
 struct MenuPrincipalView_Previews: PreviewProvider {
