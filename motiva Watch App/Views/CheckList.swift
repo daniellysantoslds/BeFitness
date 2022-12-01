80672888b35888db3a7137ee4420e0fe79ab3bf7
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2022-12-01 10:47:47 -0300

    subindo a view

---
+++ motiva Watch App/Views/CheckList.swift
@@ -0,0 +1,20 @@
+//
+//  CheckList.swift
+//  motiva Watch App
+//
+//  Created by Danielly Santos Lopes da Silva on 01/12/22.
+//
+
+import SwiftUI
+
+struct CheckList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CheckList_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckList()
+    }
+}
