e41493bc16009ecad620cae3be64dd7e269c8adf
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2022-12-20 09:28:22 -0300

    mudando nome da view Notifications para ListNotifications

--- motiva Watch App/Modelos/ListsNotifications.swift
+++ motiva Watch App/Modelos/ListsNotifications.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct Notifications: View {
+struct ListNotifications: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
@@ -15,7 +15,7 @@ struct Notifications: View {
 
 struct Notifications_Previews: PreviewProvider {
     static var previews: some View {
-        Notifications()
+        ListNotifications()
     }
 }
 
